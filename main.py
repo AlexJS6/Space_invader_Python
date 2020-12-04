67d0d9a6bc9d0498d9a6cbc6da0ccce0d23ba0f8
AlexJS6 <alex.spiesberger@gmail.com>
2020-12-05 00:48:37 +0100

    Adding ennemy space ships

--- main.py
+++ main.py
@@ -54,7 +54,21 @@ class Player(Ship): # Inheriting Ship
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img) # mask is for collision (where pixels are)
         self.max_health = health
-    
+
+class Ennemy(Ship):
+    COLOR_MAP = {
+        'red': (RED_SPACE_SHIP, RED_LASER),
+        'green': (GREEN_SPACE_SHIP, GREEN_LASER),
+        'blue': (BLUE_SPACE_SHIP, BLUE_LASER)
+    }
+
+    def __init__(self, x, y, color, health = 100): # here added color because there are different
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color] # To get right ship (check dictionnary just above)
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
 
 
 #main that makes the game run
